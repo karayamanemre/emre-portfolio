ac69c3712a5b8b7fae749002122b2a5f619e8c0e
Emre Karayaman <karayaman.emre@outlook.com>
2022-09-05 13:00:49 +0300

    add mobile menu
    added mobile menu, js linters, script.js file.

---
+++ script.js
@@ -0,0 +1,21 @@
+const menuBars = document.getElementById('menu-bars');
+const overlay = document.getElementById('overlay');
+const nav1 = document.getElementById('nav-1');
+const nav2 = document.getElementById('nav-2');
+const nav3 = document.getElementById('nav-3');
+const navItems = [nav1, nav2, nav3];
+
+function toggleNav() {
+    menuBars.classList.toggle('change');
+    overlay.classList.toggle('overlay-active');
+    if (overlay.classList.contains('overlay-active')) {
+        overlay.classList.replace('overlay-slide-left', 'overlay-slide-right');     
+    } else {
+        overlay.classList.replace('overlay-slide-right', 'overlay-slide-left'); 
+    }
+}
+
+menuBars.addEventListener('click', toggleNav);
+navItems.forEach((nav) => {
+    nav.addEventListener('click', toggleNav);
+});
