51d166a21cfce9c8d599e5719df3a04101e8f96e
Emre Karayaman <karayaman.emre@outlook.com>
2022-09-07 11:21:08 +0300

    add validation function
    add validation function for mail input in contact form

--- script.js
+++ script.js
@@ -180,3 +180,19 @@ openModal.forEach((element) => {
     });
   });
 });
+
+const email = document.getElementById('contact-email');
+const error = document.getElementById('error-message');
+const form = document.getElementById('form');
+
+error.style.visibility = 'hidden';
+
+function formValidate() {
+  if (email.value !== email.value.toLowerCase()) {
+    event.preventDefault();
+    error.style.visibility = 'visible';
+    error.innerHTML = 'Please enter your email address in lowercase.';
+  } else {
+    error.style.visibility = 'hidden';
+  }
+};
