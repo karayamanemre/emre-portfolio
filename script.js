e3943b4f5f2f7d879d8a395e5e1062adc6172483
Emre Karayaman <karayaman.emre@outlook.com>
2023-01-01 20:54:15 +0300

    style updates

--- script.js
+++ script.js
@@ -26,8 +26,7 @@ const portfolio = [
   {
     cardClass: 'works-card card1',
     cardTitle: 'Crypto Market App',
-    cardDesktopImage: 'assets/portfolio/cryptomarket-screenshot.png',
-    cardMobileImage: 'assets/portfolio/cryptomarket-screenshot.png',
+    cardImage: 'assets/portfolio/cryptomarket-screenshot.png',
     company: 'Microverse',
     job: 'Front-End',
     year: '2022',
@@ -45,8 +44,7 @@ const portfolio = [
   {
     cardClass: 'works-card card2 card-reverse',
         cardTitle: 'To Do List',
-    cardDesktopImage: 'assets/portfolio/todolist-screenshot.png',
-    cardMobileImage: 'assets/portfolio/todolist-screenshot.png',
+    cardImage: 'assets/portfolio/todolist-screenshot.png',
     company: 'Microverse',
     job: 'Front-End',
     year: '2022',
@@ -64,8 +62,7 @@ const portfolio = [
   {
     cardClass: 'works-card card3',
     cardTitle: 'Kanban Board',
-    cardDesktopImage: 'assets/portfolio/kanban-screenshot.png',
-    cardMobileImage: 'assets/portfolio/kanban-screenshot.png',
+    cardImage: 'assets/portfolio/kanban-screenshot.png',
     company: 'Freelance',
     job: 'Front-End',
     year: '2022',
@@ -83,8 +80,43 @@ const portfolio = [
   {
     cardClass: 'works-card card4 card-reverse',
     cardTitle: 'Weather App',
-    cardDesktopImage: 'assets/portfolio/weather-screenshot.png',
-    cardMobileImage: 'assets/portfolio/weather-screenshot.png',
+    cardImage: 'assets/portfolio/weather-screenshot.png',
+    company: 'Freelance',
+    job: 'Front-End',
+    year: '2022',
+    cardInfo: 'A weather app that pulls weather data from the OpenWeatherMap API to allow users to search for and view the forecast in cities worldwide.',
+    technologies: ['html', 'css', 'javascript', 'openweatherAPI'],
+    buttonText: 'See More',
+    modalInfo: 'A weather app that pulls weather data from the OpenWeatherMap API to allow users to search for and view the forecast in cities worldwide. Background changes according to weather. Built with JavaScript.',
+    modalSeeButtonText: 'See Live',
+    modalSeeButtonIcon: '',
+    modalSourceButtonText: 'See Source',
+    modalSourceButtonIcon: '',
+    modalLiveLink: 'https://karayamanemre.github.io/weather-app/',
+    modalSourceLink: 'https://github.com/karayamanemre/weather-app',
+  },
+  {
+    cardClass: 'works-card card4 card-reverse',
+    cardTitle: 'Weather App',
+    cardImage: 'assets/portfolio/weather-screenshot.png',
+    company: 'Freelance',
+    job: 'Front-End',
+    year: '2022',
+    cardInfo: 'A weather app that pulls weather data from the OpenWeatherMap API to allow users to search for and view the forecast in cities worldwide.',
+    technologies: ['html', 'css', 'javascript', 'openweatherAPI'],
+    buttonText: 'See More',
+    modalInfo: 'A weather app that pulls weather data from the OpenWeatherMap API to allow users to search for and view the forecast in cities worldwide. Background changes according to weather. Built with JavaScript.',
+    modalSeeButtonText: 'See Live',
+    modalSeeButtonIcon: '',
+    modalSourceButtonText: 'See Source',
+    modalSourceButtonIcon: '',
+    modalLiveLink: 'https://karayamanemre.github.io/weather-app/',
+    modalSourceLink: 'https://github.com/karayamanemre/weather-app',
+  },
+  {
+    cardClass: 'works-card card4 card-reverse',
+    cardTitle: 'Weather App',
+    cardImage: 'assets/portfolio/weather-screenshot.png',
     company: 'Freelance',
     job: 'Front-End',
     year: '2022',
@@ -105,29 +137,22 @@ const portfolioSection = document.getElementById('portfolio');
 
 Array.from(portfolioSection.children).forEach((child, index) => {
   child.innerHTML = `
-  <div class="${portfolio[index].cardClass}">
-    <div class="card-img card1">
-      <img class="work-img mobile" src="${portfolio[index].cardMobileImage}" alt="">
-      <img class="work-img desktop" src="${portfolio[index].cardDesktopImage}" alt="">
-    </div>
-    <div class="card-text">
-      <h2 class="card-title">${portfolio[index].cardTitle}</h2>
-      <ul class="card-details">
-        <li class="company">${portfolio[index].company}</li>
-        <li class="circle"><img src="assets/icons/circle.png" alt="circle"></li>
-        <li class="job">${portfolio[index].job}</li>
-        <li class="circle"><img src="assets/icons/circle.png" alt="circle"></li>
-        <li class="year">${portfolio[index].year}</li>
-      </ul>
-      <p class="card-info">${portfolio[index].cardInfo}</p>
-      <ul class="tags">
-        <li class="tag">${portfolio[index].technologies[0]}</li>
-        <li class="tag">${portfolio[index].technologies[1]}</li>
-        <li class="tag">${portfolio[index].technologies[2]}</li>
-      </ul>
-      <button class="btn card-button" id="${[index]}">${portfolio[index].buttonText}</button>
-    </div>
-  </div>
+    <img class='card-img' src="${portfolio[index].cardImage}" alt="">
+    <h2 class="card-title">${portfolio[index].cardTitle}</h2>
+    <ul class="card-details">
+      <li class="company">${portfolio[index].company}</li>
+      <li class="circle"><img src="assets/icons/circle.png" alt="circle"></li>
+      <li class="job">${portfolio[index].job}</li>
+      <li class="circle"><img src="assets/icons/circle.png" alt="circle"></li>
+      <li class="year">${portfolio[index].year}</li>
+    </ul>
+    <p class="card-info">${portfolio[index].cardInfo}</p>
+    <ul class="tags">
+      <li class="tag">${portfolio[index].technologies[0]}</li>
+      <li class="tag">${portfolio[index].technologies[1]}</li>
+      <li class="tag">${portfolio[index].technologies[2]}</li>
+    </ul>
+    <button class="btn card-button" id="${[index]}">${portfolio[index].buttonText}</button>
  `;
 });
 
@@ -139,44 +164,40 @@ openModal.forEach((element) => {
     modalWindow.classList.add('modal-window');
     modalWindow.innerHTML = `
     <div class="modal">
-        <div class="modal-header">
-          <h2 class="modal-title">${portfolio[element.id].cardTitle}</h2>
-          <img class="close" src="assets/icons/x-icon.png" alt="close">
-        </div>
-        <ul class="card-details">
-          <li class="company">${portfolio[element.id].company}</li>
-          <li class="circle"><img src="assets/icons/circle.png" alt="circle"></li>
-          <li class="job">${portfolio[element.id].job}</li>
-          <li class="circle"><img src="assets/icons/circle.png" alt="circle"></li>
-          <li class="year">${portfolio[element.id].year}</li>
-        </ul>
-        <div class="modal-image">
-        <img class="modal-img mobile" src="${portfolio[element.id].cardMobileImage}" alt="">
-        <img class="modal-img desktop" src="${portfolio[element.id].cardDesktopImage}" alt="">
-      </div>
-        <div class="modal-texts">
-          <p class="modal-info">${portfolio[element.id].modalInfo}</p>
-          <div class="tag-btns">
-            <ul class="modal-tags">
-              <li class="tag">${portfolio[element.id].technologies[0]}</li>
-              <li class="tag">${portfolio[element.id].technologies[1]}</li>
-              <li class="tag">${portfolio[element.id].technologies[2]}</li>
-            </ul>
-            <ul class="modal-tags">
-              <li class="github-tag tag">${portfolio[element.id].technologies[3]}</li>
-            </ul>
-            <div class="modal-btns">
-              <div class="modal-hr">
-                <a href="${portfolio[element.id].modalLiveLink}" target="_blank">
-                  <button class="btn">See Live <img src="assets/icons/live-icon.svg" alt="live"></button>
-                </a>
-                <a href="${portfolio[element.id].modalSourceLink}" target="_blank">
-                 <button class="btn">See Source <img src="assets/icons/github-icon.svg" alt="github"></button>
-                </a>
-              </div>
-            </div>  
-          <div>
-        </div>
+      <img class="close" src="assets/icons/x-icon.png" alt="close">
+      <h2 class="modal-title">${portfolio[element.id].cardTitle}</h2>
+      <ul class="card-details">
+        <li class="company">${portfolio[element.id].company}</li>
+        <li class="circle"><img src="assets/icons/circle.png" alt="circle"></li>
+        <li class="job">${portfolio[element.id].job}</li>
+        <li class="circle"><img src="assets/icons/circle.png" alt="circle"></li>
+        <li class="year">${portfolio[element.id].year}</li>
+      </ul>
+      <img class="modal-img" src="${portfolio[element.id].cardImage}" alt="">
+      <p class="modal-info">${portfolio[element.id].modalInfo}</p>
+      <ul class="modal-tags">
+        <li class="tag">${portfolio[element.id].technologies[0]}</li>
+        <li class="tag">${portfolio[element.id].technologies[1]}</li>
+        <li class="tag">${portfolio[element.id].technologies[2]}</li>
+        <li class="tag">${portfolio[element.id].technologies[3]}</li>
+      </ul>
+      <div class="modal-btns">
+        <a href="${portfolio[element.id].modalLiveLink}" target="_blank">
+          <button class="btn">
+          See Live
+          <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M2 9C2 5.13401 5.13401 2 9 2C9.55229 2 10 1.55228 10 1C10 0.447715 9.55229 0 9 0C4.02944 0 0 4.02944 0 9C0 13.9706 4.02944 18 9 18C13.9706 18 18 13.9706 18 9C18 8.44772 17.5523 8 17 8C16.4477 8 16 8.44772 16 9C16 12.866 12.866 16 9 16C5.13401 16 2 12.866 2 9ZM13 0C12.4477 0 12 0.447715 12 1C12 1.55228 12.4477 2 13 2H14.5858L8.29289 8.29289C7.90237 8.68342 7.90237 9.31658 8.29289 9.70711C8.68342 10.0976 9.31658 10.0976 9.70711 9.70711L16 3.41421V5C16 5.55228 16.4477 6 17 6C17.5523 6 18 5.55228 18 5V1C18 0.447715 17.5523 0 17 0H13Z" fill="#172a4d"/>
+          </svg>
+          </button>
+        </a>
+        <a href="${portfolio[element.id].modalSourceLink}" target="_blank">
+          <button class="btn">See Source
+          <svg width="18" height="20" viewBox="0 0 18 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M17.9989 7.46114C17.9989 6.32124 17.6329 5.25043 16.9342 4.31779C17.2004 3.41969 17.2669 2.27979 17.2004 0.967185C17.1671 0.414508 16.7345 0 16.2022 0C15.9028 0 13.3741 0.0345423 11.9101 1.38169C10.6458 1.1399 9.31495 1.1399 8.01736 1.38169C6.58667 0.0345423 4.05802 0 3.7253 0C3.19296 0 2.76042 0.414508 2.72715 0.967185C2.62734 2.27979 2.72715 3.41969 2.99333 4.31779C2.29462 5.28497 1.92863 6.35579 1.92863 7.46114C1.92863 9.8791 3.7253 12.0553 6.45358 13.0915C6.35377 13.2988 6.28723 13.5406 6.22068 13.7824C3.32604 13.4715 1.9619 10.7427 1.89536 10.639C1.66246 10.1209 1.06357 9.91364 0.56449 10.19C0.0654144 10.4318 -0.134216 11.0535 0.131958 11.5717C0.198501 11.7444 2.02845 15.4404 6.05432 15.8549V18.9637C6.05432 19.5509 6.48686 20 7.05248 20H12.875C13.4406 20 13.8732 19.5509 13.8732 18.9637V14.8532C13.8732 14.2314 13.7401 13.6442 13.5072 13.1261C16.2022 12.0553 17.9989 9.91364 17.9989 7.46114Z" fill="#172a4d"/>
+          </svg>
+        </button>
+        </a>
+      </div>  
     </div>
     `;
     document.body.appendChild(modalWindow);
